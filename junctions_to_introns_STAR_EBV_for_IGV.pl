89b0fb660652f1f2784bcf53ea8c59aa7781f74d
Tina <christina.ogrady@gmail.com>
2015-05-28 17:42:28 -0500

    junctions_to_introns_STAR_for_EBV.pl version outputs EBV and cellular junctions to different files with no depth filtering. Used to convert junctions in Rona Scott's data

---
+++ junctions_to_introns_STAR_EBV_for_IGV.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+#junctions_to_introns_STAR.pl by TO'G. Takes junctions.bed files from STAR and converts them into intron bed files separated into EBV and cellular files
+use warnings;
+use strict;
+
+	foreach my $file (@ARGV) {
+		open(INF, "<$file" ) or die "couldn't open file";
+		open(OUT, ">$file.introns_EBV_only.bed");
+		open(OUT2, ">$file.introns_cellular.bed");
+
+		my $line;
+		while( $line = <INF> ) {
+			my @cols = split( "\t", $line );	
+			
+			tr/12/+-/ foreach ($cols[3]);	#change the numeric strand indicators to + or -
+			
+			if ($cols[0] eq "chrEBV(Akata_107955to171322_1to107954)") {
+				print OUT "$cols[0]\t$cols[1]\t$cols[2]\t$cols[4]\t$cols[6]\t$cols[3]\n";
+			}
+			
+			else {
+				print OUT2 "$cols[0]\t$cols[1]\t$cols[2]\t$cols[4]\t$cols[6]\t$cols[3]\n";
+			}
+
+		}
+		
+		close(OUT2);
+		close(OUT);
+		close(INF);
+	}
