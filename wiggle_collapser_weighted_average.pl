8e73c902074d6c12efc7a867d5c6581ab35a3f79
Tina <christina.ogrady@gmail.com>
2015-06-16 17:50:43 -0500

    feature name in output now indicates range of wiggle features collapsed

--- wiggle_collapser_weighted_average.pl
+++ wiggle_collapser_weighted_average.pl
@@ -26,14 +26,22 @@ my $weighted_average_plus;
 my $weighted_average_minus;
 my $first_plus = 1;
 my $first_minus = 1;
+my @coords_plus;
+my @coords_minus;
+my $chrStart_plus;
+my $chrEnd_plus;
+my $chrStart_minus;
+my $chrEnd_minus;
 
 while (my $line = <INF>) {
 	chomp($line);
+    next if ($line =~ /^track/); #skips the track definition line
 	my @cols = split("\t", $line);
 	if ($cols[3] > 0) { #if this coordinate has a positive count...
 		if ($cols[1] < $prev_coord_plus + ($distance_between_peaks+1)) { #if the coordinate is within the specified number of bp of the previous coordinate
 			$count_sum_plus = $count_sum_plus + $cols[3]; #adds to the sums to eventually calculate the weighted average
 			$weighted_coordinate_sum_plus = $weighted_coordinate_sum_plus + ($cols[1]*$cols[3]);
+            push (@coords_plus, $cols[1]);
 			$prev_coord_plus = $cols[1]; #sets the current coordinate as the "previous coordinate" before moving on
 		}
 		else { #if the present coordinate is not within the specified number of bp of the previous coordinate, need to print out a feature
@@ -41,11 +49,15 @@ while (my $line = <INF>) {
 				$count_sum_plus = $cols[3];
 				$weighted_coordinate_sum_plus = $cols[1]*$cols[3];
 				$prev_coord_plus = $cols[1];
+                push (@coords_plus, $cols[1]);
 				$first_plus = 0;
 			}
 			else {
 				$weighted_average_plus = $weighted_coordinate_sum_plus/$count_sum_plus; #calculates weighted average
-				printf OUT "%s\t%1.0f\t%1.0f\t%d\t%d\t%s\n", "chrEBV(Akata_107955to171322_1to107954)", $weighted_average_plus, $weighted_average_plus, $count_sum_plus, $count_sum_plus, "+"; #prints out weighted average for plus strand features. Use printf to round the weighted average.
+                $chrStart_plus = $coords_plus[0];
+                $chrEnd_plus = pop(@coords_plus);
+				printf OUT "%s\t%1.0f\t%1.0f\t%d%s%d%s%d\t%d\t%s\n", "chrEBV(Akata_107955to171322_1to107954)", $weighted_average_plus, $weighted_average_plus, $chrStart_plus, ":", $chrEnd_plus, ":", $count_sum_plus, $count_sum_plus, "+"; #prints out weighted average for plus strand features. Use printf to round the weighted average.
+                @coords_plus = ($cols[1]);
 				$count_sum_plus = $cols[3]; #sets "previous coordinate", count and sum of counts for the current coordinate
 				$weighted_coordinate_sum_plus = $cols[1]*$cols[3];
 				$prev_coord_plus = $cols[1];
@@ -56,6 +68,7 @@ while (my $line = <INF>) {
 		if ($cols[1] < $prev_coord_minus + ($distance_between_peaks+1)) { #if the coordinate is within the specified number of bp of the previous coordinate
 			$count_sum_minus = $count_sum_minus + $cols[3]; #adds to the sums to eventually calculate the weighted average
 			$weighted_coordinate_sum_minus = $weighted_coordinate_sum_minus + ($cols[1]*$cols[3]);
+            push (@coords_minus, $cols[1]);
 			$prev_coord_minus = $cols[1]; #sets the current coordinate as the "previous coordinate" before moving on
 		}
 		else { #if the present coordinate is not within the specified number of bp of the previous coordinate, need to print out a feature
@@ -63,11 +76,15 @@ while (my $line = <INF>) {
 				$count_sum_minus = $cols[3];
 				$weighted_coordinate_sum_minus = $cols[1]*$cols[3];
 				$prev_coord_minus = $cols[1];
+                push (@coords_minus, $cols[1]);
 				$first_minus = 0;
 			}
 			else {
 				$weighted_average_minus = $weighted_coordinate_sum_minus/$count_sum_minus; #calculates weighted average
-				printf OUT "%s\t%1.0f\t%1.0f\t%d\t%d\t%s\n", "chrEBV(Akata_107955to171322_1to107954)", $weighted_average_minus, $weighted_average_minus, $count_sum_minus, $count_sum_minus, "-"; #prints out weighted average for plus strand features. Use printf to round the weighted average.
+                $chrStart_minus = $coords_minus[0];
+                $chrEnd_minus = pop(@coords_minus);
+				printf OUT "%s\t%1.0f\t%1.0f\t%d%s%d%s%d\t%d\t%s\n", "chrEBV(Akata_107955to171322_1to107954)", $weighted_average_minus, $weighted_average_minus, $chrStart_minus, ":", $chrEnd_minus, ":", $count_sum_minus, $count_sum_minus, "-"; #prints out weighted average for plus strand features. Use printf to round the weighted average.
+                @coords_minus = ($cols[1]);
 				$count_sum_minus = $cols[3]; #sets "previous coordinate", count and sum of counts for the current coordinate
 				$weighted_coordinate_sum_minus = $cols[1]*$cols[3];
 				$prev_coord_minus = $cols[1];
@@ -78,12 +95,16 @@ while (my $line = <INF>) {
 
 if ($count_sum_plus > 0) {#calculates and prints out weighted average for the last feature (plus strand)
 	$weighted_average_plus = $weighted_coordinate_sum_plus/$count_sum_plus;
-	printf OUT "%s\t%1.0f\t%1.0f\t%d\t%d\t%s\n", "chrEBV(Akata_107955to171322_1to107954)", $weighted_average_plus, $weighted_average_plus, $count_sum_plus,  $count_sum_plus, "+";
+    $chrStart_plus = $coords_plus[0];
+    $chrEnd_plus = pop(@coords_plus);
+	printf OUT "%s\t%1.0f\t%1.0f\t%d%s%d%s%d\t%d\t%s\n", "chrEBV(Akata_107955to171322_1to107954)", $weighted_average_plus, $weighted_average_plus, $chrStart_plus, ":", $chrEnd_plus, ":", $count_sum_plus,  $count_sum_plus, "+";
 }
 
 if ($count_sum_minus < 0) {#calculates and prints out weighted average for the last feature (minus strand)
 	$weighted_average_minus = $weighted_coordinate_sum_minus/$count_sum_minus;
-	printf OUT "%s\t%1.0f\t%1.0f\t%d\t%d\t%s\n", "chrEBV(Akata_107955to171322_1to107954)", $weighted_average_minus, $weighted_average_minus, $count_sum_minus,  $count_sum_minus, "-";
+    $chrStart_minus = $coords_minus[0];
+    $chrEnd_minus = pop(@coords_minus);
+	printf OUT "%s\t%1.0f\t%1.0f\t%d%s%d%s%d\t%d\t%s\n", "chrEBV(Akata_107955to171322_1to107954)", $weighted_average_minus, $weighted_average_minus, $chrStart_minus, ":", $chrEnd_minus, ":", $count_sum_minus,  $count_sum_minus, "-";
 }
 
 close(INF);
