b98bf15ada2be1ed70922a9b9ce8c906b0cde109
Tina <christina.ogrady@gmail.com>
2015-12-17 14:41:37 -0600

    takes an annotation file and names new transcripts with numbers that take into account existing annotation

--- Akata_transcript_namer.pl
+++ Akata_transcript_namer.pl
@@ -2,14 +2,14 @@
 # Given a bed file of transcripts aligned to the inverted Akata genome, names transcripts according to the BamHI fragment (or other supplied fragment scheme) transcription starts in, and the direction
 
 # USAGE:
-# perl /PATH/Akata_transcript_namer.pl /PATH/BamHI_fragment_bed_file /PATH/transcript_bed_file
+# perl /PATH/Akata_transcript_namer.pl /PATH/BamHI_fragment_bed_file /PATH/annotation_bed_file /PATH/transcript_bed_file
 
 #TO'G, 12/15/15
 
 use warnings;
 use strict;
 
-my ($frag_file, $tran_file) = @ARGV;
+my ($frag_file, $ann_file, $tran_file) = @ARGV;
 
 system ("awk '\$6==\"+\"' \Q$tran_file\E | sort -k 2,2n -k 3,3n > \Q$tran_file\E.plus.temp");
 system ("awk '\$6==\"-\"' \Q$tran_file\E | sort -k 3,3rn -k 2,2rn > \Q$tran_file\E.minus.temp");
@@ -27,11 +27,73 @@ while (my $line = <INF>) {
 }
 close(INF);
 
-open(INF, "<$tran_file.sorted.temp") or die "couldn't open transcript file";
-open(OUT, ">${tran_file}_named.bed") or die "couldn't open output file";
+open(INF, "<$ann_file") or die "couldn't open annotation file";
 
-my $name;
 my %names;
+my $name;
+my $name2;
+
+while (my $line = <INF>) {
+    chomp($line);
+#    next if ($line =~ /^track/); #skips the track definition line
+#    next if ($line =~ /^LMP/);
+#    next if ($line =~ /^EBNA/);
+#    next if ($line =~ /^RPMS/);
+#    
+    if ($line =~ /B\D\DF\d/) {
+        if ($line =~ /B\D\DF\d\/B\D\DF\d/) {
+            my ($prefix1, $number1, $prefix2, $number2) = $line =~ /(B\D\D)F(\d)\/(B\D\D)F(\d)/;
+            $name = "${prefix1}T";
+            $name2 = "${prefix2}T";
+            if (exists $names{$name}) {
+                if ($names{$name} > $number1) {
+                    next;
+                }
+                else {
+                    $names{$name} = $number1;
+                }
+            }
+            else {
+                $names{$name} = $number1;
+            }
+            if (exists $names{$name2}) {
+                if ($names{$name2} > $number2) {
+                    next;
+                }
+                else {
+                    $names{$name2} = $number2;
+                }
+            }
+            else {
+                $names{$name2} = $number2;
+            }
+        }
+        else {
+            my ($prefix, $number) = $line =~ /(B\D\D)F(\d)/;
+            $name = "${prefix}T";
+            if (exists $names{$name}) {
+                if ($names{$name} > $number) {
+                    next;
+                }
+                else {
+                    $names{$name} = $number;
+                }
+            }
+            else {
+                $names{$name} = $number;
+            }
+        }
+    }
+}
+
+#foreach my $hi (keys %names) {
+#    print "$hi $names{$hi}\n";
+#}
+
+close(INF);
+
+open(INF, "<$tran_file.sorted.temp") or die "couldn't open transcript file";
+open(OUT, ">${tran_file}_named_test.bed") or die "couldn't open output file";
 
 while (my $line = <INF>) {
     chomp($line);
