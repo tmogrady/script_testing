9de54b086a5c07aad63a88ac79ecaeda19996090
tmogrady <christina.ogrady@gmail.com>
2016-02-08 18:04:09 -0600

    changed name of file in usage statements

--- transcript_structure_validator.pl
+++ transcript_structure_validator.pl
@@ -4,7 +4,7 @@
 use warnings;
 use strict;
 
-die "USAGE: 'perl <PATH/transcript_structure_validator.pl> </PATH/Iso-Seq_sam_file> </PATH/validated_starts_file> </PATH/validated_ends_file> </PATH/validated_introns_file> </PATH/Annotation_bed_file>'" unless @ARGV == 5;
+die "USAGE: 'perl <PATH/TRIMD_transcript_validator.pl> </PATH/Iso-Seq_sam_file> </PATH/validated_starts_file> </PATH/validated_ends_file> </PATH/validated_introns_file> </PATH/Annotation_bed_file>'" unless @ARGV == 5;
 
 my ($test_file, $valid_starts_file, $valid_ends_file, $valid_introns_file, $ann_file) = (@ARGV);
 
