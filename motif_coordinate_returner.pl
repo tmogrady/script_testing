6a960c10baa48dda22354b01e72d0dac459c68a8
tmogrady <christina.ogrady@gmail.com>
2016-05-13 18:13:57 +0200

    works for plus and minus strand genes, as long as input file starts and ends are in ascending order

--- motif_coordinate_returner.pl
+++ motif_coordinate_returner.pl
@@ -11,6 +11,7 @@ open (OUT, ">$file.$regex.temp");
 my $flag = 0;
 my $name;
 my $chr;
+my $strand;
 my $exon;
 my @UTR_starts;
 my @UTR_ends;
@@ -25,6 +26,7 @@ while (my $line = <INF>) {
         if ($cols[4]){ #checks to see if there is coordinate information for a UTR
             $name = "$cols[1]:$cols[2]"; #creates an identifier with the transcript ID and the gene name
             $chr = $cols[3];
+            $strand = $cols[6];
             if ($cols[4] =~ /;/) { #determines if the UTR is spliced
                 @UTR_starts = split(";", $cols[4]); #extracts start positions of each UTR block
                 @UTR_ends = split(";", $cols[5]); #extracts end positions of each UTR block
@@ -46,23 +48,47 @@ while (my $line = <INF>) {
         print "@pos\n";
         foreach my $motif_start (@pos) {
         my $c_exon_size = 0;
-            for (my $i = 0; $i < scalar @UTR_starts; $i = $i + 1) { #goes through the set of exons
-                my $temp_coord = $motif_start - $c_exon_size + $UTR_starts[$i]; #calculates a temporary coordinate for the motif, assuming there is not splice junction
-                print "UTR_start: $UTR_starts[$i]\n";
-                print "temp_coord: $temp_coord\n";
-                if ($temp_coord < $UTR_ends[$i]) { #checks to see if the coordinate is in the exon
-                    print "success: $temp_coord\n\n";
-                    $chrStart = $temp_coord - 1; #convert to 0-based for bed
-                    $chrEnd = $chrStart + $motif_length;
-                    print "$chr\t$chrStart\t$chrEnd\t$name\n";
-                    print OUT "$chr\t$chrStart\t$chrEnd\t$name\n";
-                    last;
+            if ($strand == 1) {
+                for (my $i = 0; $i < scalar @UTR_starts; $i = $i + 1) { #goes through the set of exons
+                    my $temp_coord = $motif_start - $c_exon_size + $UTR_starts[$i]; #calculates a temporary coordinate for the motif, assuming there is not a splice junction
+                    print "UTR_start: $UTR_starts[$i]\n";
+                    print "temp_coord: $temp_coord\n";
+                    if ($temp_coord < $UTR_ends[$i]) { #checks to see if the coordinate is in the exon
+                        print "success: $temp_coord\n\n";
+                        $chrStart = $temp_coord - 1; #convert to 0-based for bed
+                        $chrEnd = $chrStart + $motif_length;
+                        print "$chr\t$chrStart\t$chrEnd\t$name\n";
+                        print OUT "$chr\t$chrStart\t$chrEnd\t$name\n";
+                        last;
+                    }
+                    else {
+                        $c_exon_size = $c_exon_size + $UTR_ends[$i] - $UTR_starts[$i]; #if the coordinate is not in the exon, adds the exon size to the cumulative exon size
+                        print "c_exon_size: $c_exon_size\n";
+                    }
                 }
-                else {
-                    $c_exon_size = $c_exon_size + $UTR_ends[$i] - $UTR_starts[$i]; #if the coordinate is not in the exon, adds the exon size to the cumulative exon size
-                    print "c_exon_size: $c_exon_size\n";
+            }
+            elsif ($strand == -1) { #detects genes on the negative strand
+                for (my $i = (scalar @UTR_starts)-1; $i >= 0; $i = $i - 1) { #goes through the set of exons, starting with the last one
+                    my $temp_coord = $UTR_ends[$i] - $c_exon_size - $motif_start; #calculates a temporary coordinate for the motif, assuming there is not splice junction
+                    print "UTR_start: $UTR_ends[$i]\n";
+                    print "temp_coord: $temp_coord\n";
+                    if ($temp_coord > $UTR_starts[$i]) { #checks to see if the coordinate is in the exon
+                        print "success: $temp_coord\n\n";
+                        $chrEnd = $temp_coord;
+                        $chrStart = $chrEnd - $motif_length;
+                        print "$chr\t$chrStart\t$chrEnd\t$name\n";
+                        print OUT "$chr\t$chrStart\t$chrEnd\t$name\n";
+                        last;
+                    }
+                    else {
+                        $c_exon_size = $c_exon_size + $UTR_ends[$i] - $UTR_starts[$i]; #if the coordinate is not in the exon, adds the exon size to the cumulative exon size
+                        print "c_exon_size: $c_exon_size\n";
+                    }
+                    
                 }
-                
+            }
+            else { #if there is no strand information, skip it.
+                next;
             }
         }
         @UTR_starts = ();
