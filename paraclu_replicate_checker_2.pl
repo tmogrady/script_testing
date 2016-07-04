2a76f60a3cce6121095b7fc0b2fc9cbc2dc31a72
tmogrady <christina.ogrady@gmail.com>
2016-07-04 12:30:38 +0200

    totally new strategy...

--- paraclu_replicate_checker_2.pl
+++ paraclu_replicate_checker_2.pl
@@ -5,156 +5,175 @@
 #USAGE:
 # perl PATH/paraclu_filter.pl numerical_max_distance /PATH/inputfiles
 
-#TO'G 10/28/15
+#TO'G 1/7/2016
 
 use warnings;
 use strict;
 
-my ($dist, $file1, $file2, $file3, $file4) = @ARGV;
+my ($dist, $file1, $file2) = @ARGV;
 my %peaks;
 
 open(INF, "<$file1");
-
-print "Processing first file\n";
-
-my $coord_key;
-my $values;
-my $prev_plus_chr;
-my $prev_plus_coord;
-my $coord_sum;
-my $prev_plus_count;
-my $prev_plus_dens;
-my $weighted_dens_sum;
-my $prev_plus_peak_count;
-my $plus_collapse_count;
+open(OUT, ">replicates.temp");
 
 while(my $line = <INF>) {
     chomp($line);
-    my @cols = split("\t", $line);
-    my @subcols = split("\:", $cols[3]);
-    if ($cols[5] eq "+") {
-        
-        if (($cols[0] eq $prev_chr) and ($cols[1] <= ($prev_coord + $dist))) { #if the peak is less than the specified distance from the previous peak
-            
-            $prev_plus_count = $prev_plus_count + $cols[4]; #adds tag depths
-            $plus_collapse_count = $plus_collapse_count + 1; #will be used to calculate average density & start site
-            $prev_plus_dens = (($subcols[3] + ($hashcols[1]*$hashcols[2]))/($hashcols[2]+1));
-            my $new_start = (($coord*$hashcols[0]) + ($file_start*$cols[4]))/($hashcols[0] + $cols[4]);
-            
-        }
-        else {
-            $coord_key = "$prev_plus_chr\:$prev_plus_coord\:+"; #creates a key for the hash with chr, chrStart and strand
-            $values = "$prev_plus_count\:$prev_plus_dens\:1"; #creates a value for the hash with tag count, relative density and replicate count
-            $peaks{$coord_key} = $values;
-            
-            $prev_plus_chr = $cols[0];
-            $prev_plus_coord = $cols[1];
-            $prev_plus_count = $cols[4];
-            $prev_plus_dens = $subcols[3];
-        }
-    }
-    if ($cols[5] eq "-") {
-        $coord_key = "$cols[0]\:$cols[2]\:$cols[5]"; #creates a key for the hash with chr, chrEnd and strand
-        $values = "$cols[4]\:$subcols[3]\:1"; #creates a value for the hash with tag count, relative density and replicate count
-        $peaks{$coord_key} = $values;
-    }
+    print OUT "$line\t1\n";
 }
-$coord_key = "$prev_plus_chr\:$prev_plus_coord\:+"; #creates a key for the hash with chr, chrStart and strand
-$values = "$prev_plus_count\:$prev_plus_dens\:1"; #creates a value for the hash with tag count, relative density and replicate count
-$peaks{$coord_key} = $values;
-
-
-#foreach (sort keys %peaks) {
-#    print "$_: $peaks{$_}\n";
-#}
 
 close(INF);
 
 open(INF, "<$file2");
+while(my $line = <INF>) {
+    chomp($line);
+    print OUT "$line\t2\n";
+}
 
-print "Processing second file\n";
+close(INF);
+close(OUT);
 
-my $file_start;
+system("sort -k1,1 -k2n replicates.temp > replicates_sorted.temp");
+system("rm replicates.temp");
+
+open(INF, "<replicates_sorted.temp");
+open(OUT, ">any_replicate.txt");
+open(OUT2, ">both_replicates.txt");
+
+my $prev_chrom_plus = 0;
+my $prev_coord_plus = 0;
+my $peak_sum_plus = 1;
+my $coordinate_sum_plus = 1;
+my $tag_sum_plus = 1;
+my $dens_sum_plus= 1;
+my $average_coordinate_plus = 1;
+my $average_dens_plus = 1;
+my $first_plus = 1;
+my $prev_sample_plus = 0;
+my $multiple_plus = 0;
+my $prev_chrom_minus = 0;
+my $prev_coord_minus = 0;
+my $peak_sum_minus = 1;
+my $coordinate_sum_minus = 1;
+my $tag_sum_minus = 1;
+my $dens_sum_minus= 1;
+my $average_coordinate_minus = 1;
+my $average_dens_minus = 1;
+my $first_minus = 1;
+my $prev_sample_minus = 0;
+my $multiple_minus = 0;
 
 while(my $line = <INF>) {
     chomp($line);
     my @cols = split("\t", $line);
     my @subcols = split("\:", $cols[3]);
-    my $found_flag = 0;
-    if ($cols[5] eq "+") {
-        $file_start = $cols[1];
-    }
-    if ($cols[5] eq "-") {
-        $file_start = $cols[2];
-    }
-    my $lower_limit = $file_start - $dist;
-    my $upper_limit = $file_start + $dist;
-    foreach my $hash_start (keys %peaks) {
-        my ($hash_chr, $coord, $strand) = split("\:", $hash_start);
-        if ((($hash_chr eq $cols[0]) and ($strand eq $cols[5])) and (($coord >= $lower_limit) and ($coord <= $upper_limit))) { #checks to see if the cluster is already in the hash (from the first file)
-            my @hashcols = split("\:", $peaks{$hash_start}); #extracts information about the cluster from the hash
-            my $depth = $cols[4] + $hashcols[0]; #adds tag depths from the current file and the hash
-            #my $reps = $hashcols[2]+1; #adds one to the replicate count
-            my $density = (($subcols[3] + ($hashcols[1]*$hashcols[2]))/($hashcols[2]+1));
-            my $new_start = (($coord*$hashcols[0]) + ($file_start*$cols[4]))/($hashcols[0] + $cols[4]);
-
-            $coord_key = "$cols[0]\:$new_start\:$cols[5]";
-            $values = "$depth\:$density\:2";
-            delete $peaks{$hash_start};
-            $peaks{$coord_key} = $values;
-            $found_flag = 1;
-            last;
+    if ($cols[5] eq "+") { #plus strand
+        if (($cols[0] eq $prev_chrom_plus) and ($cols[1] <= ($prev_coord_plus + $dist))) { #if the coordinate is within the specified distance of the previous one
+            $peak_sum_plus = $peak_sum_plus + 1; #counts number of peaks within the window to use in average calculation
+            $coordinate_sum_plus = $coordinate_sum_plus + $cols[1]; #adds the coordinate to the sum used to calculate the average position
+            $tag_sum_plus = $tag_sum_plus + $cols[4]; #sums the number of CAGE tags contributing to the site
+            $dens_sum_plus = $dens_sum_plus + $subcols[3]; #sums the densities of the peaks within to window to eventually get average
+            $prev_coord_plus = $cols[1];
+            if ($cols[6] != $prev_sample_plus) { #if this peak is from a different sample than the previous one, flips the switch to note that
+                $multiple_plus = 1;
+            }
+            $prev_sample_plus = $cols[6];
+        }
+        else {
+            if ($first_plus == 1) { #"first" flag avoids wonkiness if the first coordinate is far from coordinate 1 (don't need to print out a feature yet)
+                $prev_chrom_plus = $cols[0];
+                $prev_coord_plus = $cols[1];
+                $peak_sum_plus = 1;
+                $coordinate_sum_plus = $cols[1];
+                $tag_sum_plus = $cols[4];
+                $dens_sum_plus = $subcols[3];
+                $prev_sample_plus = $cols[6];
+                $first_plus = 0;
+            }
+            else {
+                $average_coordinate_plus = sprintf("%1.0f", ($coordinate_sum_plus/$peak_sum_plus)); #calculates coordinate average
+                $average_dens_plus = ($dens_sum_plus/$peak_sum_plus); #calculates average density
+                if ($multiple_plus == 1) {
+                    print OUT $prev_chrom_plus, "\t", $average_coordinate_plus, "\t", $average_coordinate_plus+1, "\t", $average_dens_plus, ":", $tag_sum_plus, ":2\t", $tag_sum_plus, "\t+\n";
+                    print OUT2 $prev_chrom_plus, "\t", $average_coordinate_plus, "\t", $average_coordinate_plus+1, "\t", $average_dens_plus, ":", $tag_sum_plus, ":2\t", $tag_sum_plus, "\t+\n";
+                }
+                else {
+                    print OUT $prev_chrom_plus, "\t", $average_coordinate_plus, "\t", $average_coordinate_plus+1, "\t", $average_dens_plus, ":", $tag_sum_plus, ":1\t", $tag_sum_plus, "\t+\n";
+                }
+                $prev_chrom_plus = $cols[0];
+                $prev_coord_plus = $cols[1];
+                $peak_sum_plus = 1;
+                $coordinate_sum_plus = $cols[1];
+                $tag_sum_plus = $cols[4];
+                $dens_sum_plus = $subcols[3];
+                $prev_sample_plus = $cols[6];
+                $multiple_plus = 0;
+            }
         }
     }
-    if ($found_flag == 0) {
-        $coord_key = "$cols[0]\:$file_start\:$cols[5]";
-        $values = "$cols[4]\:$subcols[3]\:1";
-        $peaks{$coord_key} = $values; #adds the coordinate to the hash as a key, with the value assigned above as a value
+    elsif ($cols[5] eq "-"){
+        if (($cols[0] eq $prev_chrom_minus) and ($cols[2] <= ($prev_coord_minus + $dist))) {
+            $peak_sum_minus = $peak_sum_minus + 1; #counts number of peaks within the window to use in average calculation
+            $coordinate_sum_minus = $coordinate_sum_minus + $cols[1]; #adds the coordinate to the sum used to calculate the average position
+            $tag_sum_minus = $tag_sum_minus + $cols[4]; #sums the number of CAGE tags contributing to the site
+            $dens_sum_minus = $dens_sum_minus + $subcols[3]; #sums the densities of the peaks within to window to eventually get average
+            $prev_coord_minus = $cols[2];
+            if ($cols[6] != $prev_sample_minus) { #if this peak is from a different sample than the previous one, flips the switch to note that
+                $multiple_minus = 1;
+            }
+            $prev_sample_minus = $cols[6];
+        }
+        else {
+            if ($first_minus == 1) { #"first" flag avoids wonkiness if the first coordinate is far from coordinate 1 (don't need to print out a feature yet)
+                $prev_chrom_minus = $cols[0];
+                $prev_coord_minus = $cols[2];
+                $peak_sum_minus = 1;
+                $coordinate_sum_minus = $cols[2];
+                $tag_sum_minus = $cols[4];
+                $dens_sum_minus = $subcols[3];
+                $prev_sample_minus = $cols[6];
+                $first_minus = 0;
+            }
+            else {
+                $average_coordinate_minus = sprintf("%1.0f", ($coordinate_sum_minus/$peak_sum_minus)); #calculates coordinate average
+                $average_dens_minus = ($dens_sum_minus/$peak_sum_minus); #calculates average density
+                if ($multiple_minus == 1) {
+                    print OUT $prev_chrom_minus, "\t", $average_coordinate_minus-1, "\t", $average_coordinate_minus, "\t", $average_dens_minus, ":", $tag_sum_minus, ":2\t", $tag_sum_minus, "\t-\n";
+                    print OUT2 $prev_chrom_minus, "\t", $average_coordinate_minus-1, "\t", $average_coordinate_minus, "\t", $average_dens_minus, ":", $tag_sum_minus, ":2\t", $tag_sum_minus, "\t-\n";
+                }
+                else {
+                    print OUT $prev_chrom_minus, "\t", $average_coordinate_minus-1, "\t", $average_coordinate_minus, "\t", $average_dens_minus, ":", $tag_sum_minus, ":1\t", $tag_sum_minus, "\t-\n";
+                }
+                $prev_chrom_minus = $cols[0];
+                $prev_coord_minus = $cols[2];
+                $peak_sum_minus = 1;
+                $coordinate_sum_minus = $cols[2];
+                $tag_sum_minus = $cols[4];
+                $dens_sum_minus = $subcols[3];
+                $prev_sample_minus = $cols[6];
+                $multiple_minus = 0;
+            }
+        }
     }
-    
+}
+
+#need to deal with last features too:
+
+if ($multiple_plus == 1) {
+    print OUT $prev_chrom_plus, "\t", $average_coordinate_plus, "\t", $average_coordinate_plus+1, "\t", $average_dens_plus, ":", $tag_sum_plus, ":2\t", $tag_sum_plus, "\t+\n";
+    print OUT2 $prev_chrom_plus, "\t", $average_coordinate_plus, "\t", $average_coordinate_plus+1, "\t", $average_dens_plus, ":", $tag_sum_plus, ":2\t", $tag_sum_plus, "\t+\n";
+}
+else {
+    print OUT $prev_chrom_plus, "\t", $average_coordinate_plus, "\t", $average_coordinate_plus+1, "\t", $average_dens_plus, ":", $tag_sum_plus, ":1\t", $tag_sum_plus, "\t+\n";
+}
+
+if ($multiple_minus == 1) {
+    print OUT $prev_chrom_minus, "\t", $average_coordinate_minus-1, "\t", $average_coordinate_minus, "\t", $average_dens_minus, ":", $tag_sum_minus, ":2\t", $tag_sum_minus, "\t-\n";
+    print OUT2 $prev_chrom_minus, "\t", $average_coordinate_minus-1, "\t", $average_coordinate_minus, "\t", $average_dens_minus, ":", $tag_sum_minus, ":2\t", $tag_sum_minus, "\t-\n";
+}
+else {
+    print OUT $prev_chrom_minus, "\t", $average_coordinate_minus-1, "\t", $average_coordinate_minus, "\t", $average_dens_minus, ":", $tag_sum_minus, ":1\t", $tag_sum_minus, "\t-\n";
 }
 
 close(INF);
-
-open(OUT, ">any_replicate.temp");
-open(OUT2, ">both_replicates.temp");
-open(OUT5, ">weird_replicates.temp");
-
-my $chrStart;
-my $chrEnd;
-my $out_line;
-
-foreach my $hash_start (sort keys %peaks) {
-    my ($chr, $coord, $strand) = split("\:", $hash_start);
-    my @hashcols = split("\:", $peaks{$hash_start});
-    #print "$chr\t$coord\t$strand\n";
-    if ($strand eq "+") {
-        $chrStart = sprintf("%1.0f", $coord);
-        $chrEnd = $chrStart + 1;
-        $out_line = "$chr\t$chrStart\t$chrEnd\t$peaks{$hash_start}\t$hashcols[0]\t$strand\n";
-    }
-    if ($strand eq "-") {
-        $chrEnd = sprintf("%1.0f", $coord);
-        $chrStart = $chrEnd - 1;
-        $out_line = "$chr\t$chrStart\t$chrEnd\t-$peaks{$hash_start}\t$hashcols[0]\t$strand\n";
-    }
-    print OUT $out_line;
-    if ($hashcols[2] == 2) {
-        print OUT2 $out_line;
-    }
-    if ($hashcols[2] > 2) {
-        print OUT5 $out_line;
-    }
-}
-
-system("sort -k1,1 -k2n any_replicate.temp > any_replicate.bed");
-system("sort -k1,1 -k2n both_replicates.temp > both_replicates.bed");
-system("sort -k1,1 -k2n weird_replicates.temp > weird_replicates.bed");
-
-system("rm any_replicate.temp");
-system("rm both_replicates.temp");
-system("rm weird_replicates.temp");
-
 close(OUT);
 close(OUT2);
-close(OUT5);
